a765b4eb12fa72c13f8faae621e7e2b9f0364e37
Abdusaid10 <36532487+Abdusaid10@users.noreply.github.com>
2018-05-12 18:50:46 +0500

    Add files via upload

---
+++ bellissimo/public/app/popup.js
@@ -0,0 +1,38 @@
+// Get the modal
+var modal = document.getElementById('popup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "block";
+    }
+}
+
+// eye-> to make the password visible
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+var pwShown = 0;
+var NAME = document.getElementById("eye");
+document.getElementById("eye").addEventListener("click", function () {
+     var currentClass = NAME.className;
+    if (currentClass == "fa fa-eye-slash") { // Check the current class name
+        NAME.className = "fa fa-eye";   // Set other class name
+    } else {
+        NAME.className = "fa fa-eye-slash";  // Otherwise, use `second_name`
+    }
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    	
+    }
+}, false);
